33e9c2e1c247495d5567bb7181f74cb7af4080f2
jhondev-code <jhondev.code@gmail.com>
2021-04-02 13:37:36 -0500

    formatters code has been updated
    Supports paragraph formatting

--- formatters.py
+++ formatters.py
@@ -1,13 +1,31 @@
 from impl import AbstractFormatter
 
 
+def available_character(character: str):
+    return 48 <= ord(character) <= 57 or 65 <= ord(character) <= 122
+
+
 class PlainTextFormatter(AbstractFormatter):
     """Provides a formatter for plain texts, rearranging by paragraphs and removing strange characters from the text"""
-    
+
     def __init__(self):
         """Default constructor"""
         super(PlainTextFormatter, self).__init__()
 
     def format(self, text: str) -> str:
         """Format text, clean up weird characters and rearrange it"""
-        return text.replace("\r", "").replace("\n", "")
+        old_text = text.replace("\r", "")
+        new_text = ""
+        counter = 0
+        for character in text:
+            if character == "\n":
+                if old_text[counter - 1] == ".":
+                    new_text += "\n\n"
+                if old_text[counter - 1] == ":":
+                    new_text += "\n"
+            elif character == '-':
+                new_text += ""
+            else:
+                new_text += character
+            counter += 1
+        return new_text
