6fc0ebf884f95e1a479f658c8049e63e3e5f5baa
jhondevcode <jhondev.code@gmail.com>
2021-09-27 22:13:36 -0500

    Logger module is depecrated

--- src/logger.py
+++ src/logger.py
@@ -1,29 +1,14 @@
-import datetime
-import logging
-import os
-from util import WORKSPACE
-
-LOG_DIR = os.path.join(WORKSPACE, "logs")
-
-if not os.path.isdir(LOG_DIR):
-    os.mkdir(LOG_DIR)
-
-log_file = os.path.join(LOG_DIR, f"log-{datetime.datetime.now().date()}.log")
-logging.basicConfig(filename=log_file, format='%(asctime)s %(message)s', filemode='a')
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-
 def info(msg, *args, **kwargs):
-    logger.info(msg, args, kwargs)
+    pass
 
 
 def warn(msg, *args, **kwargs):
-    logger.warning(msg, args, kwargs)
+    pass
 
 
 def error(msg, *args, **kwargs):
-    logger.error(msg, args, kwargs)
+    pass
 
-def log(ex):
-    error(ex)
+
+def log(exeption):
+    pass
